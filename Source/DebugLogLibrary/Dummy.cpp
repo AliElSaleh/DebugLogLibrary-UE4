3ebda7e69531a64c6cf7cc6ad371760839fb66ba
Ali El Saleh <elsaleh78@gmail.com>
2020-03-07 11:28:06 +1100

    Update test code

--- Source/DebugLogLibrary/Dummy.cpp
+++ Source/DebugLogLibrary/Dummy.cpp
@@ -1,8 +1,9 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "Dummy.h"
+#if !UE_BUILD_SHIPPING
 #include "Log.h"
-#include "Engine/Engine.h"
+#endif
 
 ADummy::ADummy()
 {
@@ -15,12 +16,7 @@ void ADummy::BeginPlay()
 	Super::BeginPlay();
 
 #if !UE_BUILD_SHIPPING
-	//const FCapsuleShape Capsule{GetActorLocation(), 5.0f, FVector::UpVector, 20.0f};
-	//const FSphere Sphere{GetActorLocation(), 5.0f};
-	const FBox Box{GetActorLocation() - FVector(10.0f), GetActorLocation() + FVector(10.0f)};
-	//ULog::Capsule(Capsule, "[Begin My Capsule]", "[End My Capsule]", LO_Both);
-	//ULog::Sphere(Sphere, "[Begin My Sphere]", "[End My Sphere]", LO_Both);
-	ULog::Box(Box, "[Begin My Box]", "[End My Box]", LO_Both);
+	ULog::Hello(LO_Both);
 #endif
 }
 
