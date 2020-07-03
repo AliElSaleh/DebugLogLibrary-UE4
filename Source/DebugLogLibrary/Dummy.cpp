b5c764ccfb6397bd89c5ba384100f42510a7c520
Ali El Saleh <elsaleh78@gmail.com>
2020-07-03 19:05:08 +1000

    Update Dummy.cpp

--- Source/DebugLogLibrary/Dummy.cpp
+++ Source/DebugLogLibrary/Dummy.cpp
@@ -13,45 +13,51 @@ void ADummy::BeginPlay()
 {
 	Super::BeginPlay();
 
-	const FVector A(1.0f);
-	const FColor B(1.0f);
-	const FRotator C(1.0f);
-	const FQuat D(1.0f, 1.0f, 1.0f, 1.0f);
-	const FVector2D E(1.0f);
+	{
+		const FVector A(1.0f);
+		const FColor B(1.0f);
+		const FRotator C(1.0f);
+		const FQuat D(1.0f, 1.0f, 1.0f, 1.0f);
+		const FVector2D E(1.0f);
 
-	LOG_VECTOR(A);
-	LOG_VECTOR_COMPACT(A, "Vector Compact: ", "");
-	LOG_VECTOR(A, "Vector: ", "");
+		LOG_VECTOR(A);
+		LOG_VECTOR_COMPACT(A, "Vector Compact: ", "");
+		LOG_VECTOR(A, "Vector: ", "");
+	}
+
+	ULog::Info(CUR_CLASS_FUNC_2 + "Damn this is cool dawg!");
 	
-	LOG_LINEBREAK();
-	
-	LOG_VECTOR2D(E);
-	LOG_VECTOR2D_COMPACT(E);
-
-	LOG_LINEBREAK();
-	
-	LOG_COLOR(B);
-	LOG_COLOR_COMPACT(B);
-
-	LOG_LINEBREAK();
-
-	LOG_ROTATOR(C);
-	LOG_ROTATOR_COMPACT(C);
-
-	LOG_LINEBREAK();
-
-	LOG_QUAT(D);
-	LOG_QUAT_COMPACT(D);
+	//LOG_LINEBREAK();
+	//
+	//LOG_VECTOR2D(E);
+	//LOG_VECTOR2D_COMPACT(E);
+//
+	//LOG_LINEBREAK();
+	//
+	//LOG_COLOR(B);
+	//LOG_COLOR_COMPACT(B);
+//
+	//LOG_LINEBREAK();
+//
+	//LOG_ROTATOR(C);
+	//LOG_ROTATOR_COMPACT(C);
+//
+	//LOG_LINEBREAK();
+//
+	//LOG_QUAT(D);
+	//LOG_QUAT_COMPACT(D);
 
 	LOG_LINEBREAK();
 	LOG_LINEBREAK_S(-------------);
 	LOG_LINEBREAK();
-	
+
+	FString B = ENUM_TO_STRING(ELoggingOptions, LO_Console, true);
+
 	LOG_ENUM(EAxis::Type, EAxis::X, true, "Axis: ", "");
-	LOG_ENUM(ELoggingOptions, ELoggingOptions::LO_Both, true);
+	LOG_ENUM(ELoggingOptions, LO_Both, true);
 }
 
-void ADummy::Tick(float DeltaTime)
+void ADummy::Tick(const float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
